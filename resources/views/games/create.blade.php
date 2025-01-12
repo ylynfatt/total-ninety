dc07bdc54f689ad11f06d358bac06e88269d6e57
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2025-01-12 10:59:54 -0500

    Update Button Text

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -30,6 +30,6 @@
         <label for="location" class="block mb-2 text-sm font-medium text-gray-900">Location</label>
         <input type="text" name="location" id="location" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Add Team</button>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Add Game</button>
 </form>
 @endsection
